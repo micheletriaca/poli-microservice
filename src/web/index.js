0a16179ac0431eb4f14645497949a6068df1354d
Michele Triaca <michele.triaca@telnext.com>
2019-11-04 22:11:22 +0100

    [throng] added throng to span 1 process per available cpu

--- src/web/index.js
+++ src/web/index.js
@@ -1,11 +1,20 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const apiRouter = require('./api-router')
+const throng = require('throng')
 const PORT = process.env.PORT || 8080
+const WORKERS = process.env.WEB_CONCURRENCY || 1
 
-express()
-  .use(morgan('tiny'))
-  .use('/api', apiRouter)
-  .get('/', (req, res) => res.json({ status: 'UP AND RUNNIG' }))
-  .get('*', (req, res) => res.status(404).json({ status: 'NOT FOUND' }))
-  .listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`))
+throng({
+  workers: WORKERS,
+  start: id => {
+    console.log(`Started thread with id ${id}`)
+    express()
+      .use(morgan('tiny'))
+      .use('/api', apiRouter)
+      .get('/', (req, res) => res.json({ status: 'UP AND RUNNIG' }))
+      .get('*', (req, res) => res.status(404).json({ status: 'NOT FOUND' }))
+      .listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`))
+  }
+})
